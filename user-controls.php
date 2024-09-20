1cad88e2e35931dad7549c8477c0622b7c476ca9
1Kr3ksu <Kacperbielawski33@gmail.com>
2024-09-20 07:17:11 +0200

    up

--- user-controls.php
+++ user-controls.php
@@ -1,13 +1,13 @@
 <?php
 include "db_connection.php";
-session_start(); // Zainicjuj sesję
+session_start(); 
 
-// Sprawdź, czy użytkownik jest zalogowany
+
 if (isset($_SESSION['user_id'])) {
-    // Użytkownik zalogowany, pokaż przycisk "Wyloguj"
+  
     echo '<button class="Log" onclick="window.location.href=\'logout.php\';">Wyloguj</button>';
 } else {
-    // Użytkownik niezalogowany, pokaż przycisk "Logowanie"
+ 
     echo '<button class="Log" onclick="window.location.href=\'Login.php\';">Logowanie</button>';
 }
 ?>
